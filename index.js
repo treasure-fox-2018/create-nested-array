84d4dfd234fb83fe9e9b8ddb92fce18e160fe4eb
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-30 12:37:33 +0700

    exercise done !

---
+++ index.js
@@ -0,0 +1,16 @@
+function createNestedArray(jumlahRow, jumlahCol){
+    var arr = []
+    var temp = []
+    var alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for(var i = 0; i < jumlahRow; i++){
+        for(var j = 0; j < jumlahCol; j++){
+            temp.push(alpha[Math.floor(Math.random()*26)])
+        }
+        arr.push(temp)
+        temp = []
+    }
+    console.log(arr);
+    
+}
+
+console.log(createNestedArray(4,2));
