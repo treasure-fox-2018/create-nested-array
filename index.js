176fd4cbcee39e5096237f144acc4fbd3f4a12a1
fajartc02 <fajartc02@gmail.com>
2018-05-30 10:36:14 +0700

    submit create-nested-array completed

---
+++ index.js
@@ -0,0 +1,22 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+// console.log(getRandomInt(0, 26))
+
+function createNestedArr(jumlahRow, jumlahCol) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  var mainArr = []
+  // console.log(count)
+  for (var a = 0; a < jumlahRow; a++) {
+    var strArr = []
+    mainArr.push(strArr)
+    for (var b = 0; b < jumlahCol; b++) {
+      var count = getRandomInt(0, 26)
+      var str = alphabet[count]
+      mainArr[a].push(str)
+    }
+  }
+  return mainArr
+}
+
+console.log(createNestedArr(5, 3))
